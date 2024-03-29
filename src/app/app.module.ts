b84b30f80a3e7a7af94a2643c05d4f92a4f33c9d
@ismaelnv <jonasvillarruelnegrete@gmail.com>
2024-03-29 13:42:54 -0500

    Barra de navegacion terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarraDeNavegacionComponent } from './Component/barra-de-navegacion/barra-de-navegacion.component';
+import { CategoriaComponent } from './Component/categoria/categoria.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarraDeNavegacionComponent
+    BarraDeNavegacionComponent,
+    CategoriaComponent
   ],
   imports: [
     BrowserModule,
